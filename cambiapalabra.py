a64fb1824ce993d48a369d5aaadef19bf1c62c5b
Marcelo Hornillos <marcehornillos@gmail.com>
2021-07-16 22:40:17 +0200

    práctica final con ejercicios para el lunes

--- cambiapalabra.py
+++ cambiapalabra.py
@@ -2,9 +2,22 @@ import sys
 
 argumentos = sys.argv
 
+if len(argumentos) < 2:
+    argumentos.append(input("Introduce el nombre del fichero: "))
+    argumentos.append(input("Introduce la palabra original: "))
+    argumentos.append(input("Introduce la nueva palabra: "))
+elif len(argumentos) < 3:
+    argumentos.append(input("Introduce la palabra original: "))
+    argumentos.append(input("Introduce la nueva palabra: "))
+elif len(argumentos) < 4:
+    argumentos.append(input("Introduce la nueva palabra: "))
+
+
 nombreF = argumentos[1]
 original = argumentos[2]
 nueva = argumentos[3]
+
+
 
 
 nombreF = "fichero.txt"

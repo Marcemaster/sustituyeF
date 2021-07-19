e74cc981fcaf92e460cd6c602d59748142cf4279
Marcelo Hornillos <marcehornillos@gmail.com>
2021-07-19 21:49:27 +0200

    Correcciones ejercicios

--- cambiapalabra.py
+++ cambiapalabra.py
@@ -4,21 +4,16 @@ argumentos = sys.argv
 
 if len(argumentos) < 2:
     argumentos.append(input("Introduce el nombre del fichero: "))
-    argumentos.append(input("Introduce la palabra original: "))
-    argumentos.append(input("Introduce la nueva palabra: "))
-elif len(argumentos) < 3:
-    argumentos.append(input("Introduce la palabra original: "))
-    argumentos.append(input("Introduce la nueva palabra: "))
-elif len(argumentos) < 4:
-    argumentos.append(input("Introduce la nueva palabra: "))
 
+if len(argumentos) < 3:
+    argumentos.append(input("Introduce la palabra original: "))
+
+if len(argumentos) < 4:
+    argumentos.append(input("Introduce la nueva palabra: "))
 
 nombreF = argumentos[1]
 original = argumentos[2]
 nueva = argumentos[3]
-
-
-
 
 nombreF = "fichero.txt"
 
@@ -31,5 +26,3 @@ texto_nuevo = texto_original.replace(original, nueva)
 f = open(nombreF, "w")
 f.write(texto_nuevo)
 f.close()
-
-
